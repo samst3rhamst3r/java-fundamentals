076bacee1683564c17a2925f9d86250daaf369c0
Sam Turner <3241671+samst3rhamst3r@users.noreply.github.com>
2024-03-27 16:36:24 -0700

    Fundamentals exercises and .gitignore

--- src/labs_examples/arrays/labs/Exercise_03.java
+++ src/labs_examples/arrays/labs/Exercise_03.java
@@ -3,7 +3,7 @@ package labs_examples.arrays.labs;
 /**
  *  2D Array
  *
- *      Creat and populate a 5x5 2D array with multiples of 3, starting with 3. Once populated, print out the results.
+ *      Create and populate a 5x5 2D array with multiples of 3, starting with 3. Once populated, print out the results.
  *
  *      The output should look something like this:
  *
@@ -18,6 +18,6 @@ package labs_examples.arrays.labs;
 public class Exercise_03 {
 
     public static void main(String[] args) {
-        
+        System.out.println("Wow");
     }
 }
--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -12,8 +12,21 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
+        boolean bool = true;
+        byte b = 10;
+        short s = 25000;
         int i = 1;
+        long l = 1000000;
+        float f = 12.34F;
+        double d = 1234.5854849;
+
+        System.out.println("boolean bool is: " + bool);
+        System.out.println("byte b is: " + b);
+        System.out.println("short s is: " + s);
         System.out.println("int i is: " + i);
+        System.out.println("long l is: " + l);
+        System.out.println("float f is: " + f);
+        System.out.println("double d is: " + d);
 
         // write your code below
 
